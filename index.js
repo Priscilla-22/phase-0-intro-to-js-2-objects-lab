8d868f3b3341436ddfc823967bb58d15a8e0b61e
Priscilla-22 <priscillamw123@gmail.com>
2023-12-08 20:10:03 +0300

    Completed assognment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+const employee =  {
+    name: "Priscilla", 
+    streetAddress: "Tom Mboya"
+}
+
+/*function should take in three arguments ,return a new Object that has an updated value for the key passed in. */
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+const updateEmployee = updateEmployeeWithKeyAndValue(employee, "name", "George");
+console.log(updateEmployee);
+console.log(employee);
+
+//this function mutates the employee Object passed in.
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+const updatedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "Kimathi street");
+console.log(updatedEmployee);
+console.log(employee);
+
+
+/* this function deletes the property with that key from the employee Object and does not mutate the original employee Object */
+function deleteFromEmployeeByKey(employee, key, value) {
+    return {...employee, [key]:value}
+}
+const noNameEmployee = deleteFromEmployeeByKey(employee, "name", "Priscilla")
+delete noNameEmployee.name;
+console.log(noNameEmployee);
+console.log(employee);
+
+/*this function mutates the employee Object. */
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee
+}
+destructivelyDeleteFromEmployeeByKey(employee, "name");
+console.log(employee);
